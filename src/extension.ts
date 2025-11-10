fdc0f1012f5e885bbbf58906795241ee0332567e
qiuye-zhou <zhouzw1302@163.com>
2025-11-10 21:12:00 +0800

    chore: update

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import { activate as backgroundActivate } from './activates/background'
 
 // 扩展被激活时调用此方法
 export function activate(context: vscode.ExtensionContext) {
-  // 扩展被激活时执行一次
   console.log('扩展 "qy-vscode-plugin" 已经激活！')
 
   quickGitActivate(context)
@@ -13,5 +12,5 @@ export function activate(context: vscode.ExtensionContext) {
 
 // 扩展被停用时调用此方法
 export function deactivate() {
-  console.log('插件已经被释放')
+  console.log('插件 "qy-vscode-plugin" 已经被释放')
 }
